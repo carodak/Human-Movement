d8b53d640da064039d9e0a2ab91695862271f018
Helgi Tomas Gislason <helgso@gmail.com>
2018-11-14 21:59:54 -0500

    Structuring the repo for all three datasets and all three classifiers

---
+++ MPII/Classifiers/kNN.py
@@ -0,0 +1,9 @@
+
+
+# A kNN classifier for the dataset MPII
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ Stanford/Classifiers/AdaBoost.py
@@ -0,0 +1,21 @@
+# Load the pickle file.
+import pickle
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import AdaBoostClassifier
+
+
+# An AdaBoost classifier for the Stanford dataset
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
