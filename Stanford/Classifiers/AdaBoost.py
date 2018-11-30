47e0ec9bfe0395a4bb2679bdee681143cc373d47
carodak <carodak2@gmail.com>
2018-11-29 23:25:23 -0500

    AdaBoost Stanford

--- Stanford/Classifiers/AdaBoost.py
+++ Stanford/Classifiers/AdaBoost.py
@@ -22,7 +22,6 @@ labels = pickle.load( open( parent_parent_dir+"/Stanford/Data/stanford_labels.p"
 #Change the list to an array
 X = np.array([x.tolist() for x in dataset])
 #print(X)
-#print(X.shape)
 
 n = X.shape[0]
 
@@ -33,7 +32,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.33, r
 #Follow this tutorialhttps://code-examples.net/fr/docs/scikit_learn/modules/cross_validation
 #scaler = preprocessing.StandardScaler().fit(X_train)
 #X_train_transformed = scaler.transform(X_train)
-clf = AdaBoostClassifier(n_estimators=1000,learning_rate=0.0001)
+clf = AdaBoostClassifier(n_estimators=10,learning_rate=1)
 #X_test_transformed = scaler.transform(X_test)
 clf.fit(X_train,y_train)
 print(clf.score(X_train, y_train))
